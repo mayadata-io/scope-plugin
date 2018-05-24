33c2cadf19c60d08a4b37b680fd66e54b9a6891d
Pradeepkumarbk <pradeepkumar.bk96@gmail.com>
2018-05-24 19:20:58 +0530

    Add read and write iops to PV
    - This commit will add read and write iops graph to PV
    - This will update the Cortex-agent url in yaml
    
    Signed-off-by: Pradeepkumarbk <pradeepkumar.bk96@gmail.com>

--- iops/main.go
+++ iops/main.go
@@ -3,154 +3,27 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
 	"os"
 	"os/signal"
 	"path/filepath"
-	"strings"
-	"sync"
 	"syscall"
 	"time"
 
-	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 )
 
-func setupSocket(socketPath string) (net.Listener, error) {
-	os.RemoveAll(filepath.Dir(socketPath))
-	if err := os.MkdirAll(filepath.Dir(socketPath), 0700); err != nil {
-		return nil, fmt.Errorf("failed to create directory %q: %v", filepath.Dir(socketPath), err)
-	}
-	listener, err := net.Listen("unix", socketPath)
-	if err != nil {
-		return nil, fmt.Errorf("failed to listen on %q: %v", socketPath, err)
-	}
-
-	log.Printf("Listening on: unix://%s", socketPath)
-	return listener, nil
-}
-
-func setupSignals(socketPath string) {
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-interrupt
-		os.RemoveAll(filepath.Dir(socketPath))
-		os.Exit(0)
-	}()
-}
-
-// Iops struct
-type Iops struct {
-	Status string `json:"status"`
-	Data   struct {
-		ResultType string `json:"resultType"`
-		Result     []struct {
-			Metric struct {
-				Name              string `json:"__name__"`
-				Instance          string `json:"instance"`
-				Job               string `json:"job"`
-				KubernetesPodName string `json:"kubernetes_pod_name"`
-				OpenebsPv         string `json:"openebs_pv"`
-			} `json:"metric"`
-			Value []interface{} `json:"value"`
-		} `json:"result"`
-	} `json:"data"`
-}
-
-func getValue(body []byte) (*Iops, error) {
-	var s = new(Iops)
-	err := json.Unmarshal(body, &s)
-	if err != nil {
-		fmt.Println("whoops:")
-	}
-	return s, err
-}
-func main() {
-	// creates the in-cluster config
-	config, err := rest.InClusterConfig()
-	if err != nil {
-		panic(err.Error())
-	}
-	// creates the clientset
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	//we put the in a sub-directory to have more control on the permissions
-	const socketPath = "/var/run/scope/plugins/iowait/iowait.sock"
-	url := os.Getenv("CORTEXAGENT")
-
-	// Get request to url
-	res, err := http.Get(strings.TrimSpace(url))
-	if err != nil {
-		log.Println(err.Error())
-		return
-	}
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		log.Println(err.Error())
-	}
-
-	s, err := getValue([]byte(body))
-
-	// Handle the exit signal
-	setupSignals(socketPath)
-
-	listener, err := setupSocket(socketPath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer func() {
-		listener.Close()
-		os.RemoveAll(filepath.Dir(socketPath))
-	}()
-	log.Printf("pr: %v", s)
-
-	meta, err := clientset.CoreV1().PersistentVolumes().Get(s.Data.Result[0].Metric.OpenebsPv, metav1.GetOptions{})
-	if errors.IsNotFound(err) {
-		fmt.Printf("Pod not found\n")
-	} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
-		fmt.Printf("Error getting pod %v\n", statusError.ErrStatus.Message)
-	} else if err != nil {
-		panic(err.Error())
-	} else {
-		log.Printf("Found pod\n")
-	}
-
-	plugin, err := NewPlugin(string(meta.UID))
-	if err != nil {
-		log.Fatalf("Failed to create a plugin: %v", err)
-	}
-	http.HandleFunc("/report", plugin.Report)
-	if err := http.Serve(listener, nil); err != nil {
-		log.Printf("error: %v", err)
-	}
-}
-
-// NewPlugin instantiates a new plugin
-func NewPlugin(pv string) (*Plugin, error) {
-	pvID := pv
-	plugin := &Plugin{
-		PersistentVolumeID: pvID,
-	}
-	return plugin, nil
-}
+var url string
+var clientset *kubernetes.Clientset
+var readch chan map[string]float64
+var writech chan map[string]float64
 
 // Plugin groups the methods a plugin needs
 type Plugin struct {
-	pvid               string
-	PersistentVolumeID string
-
-	lock       sync.Mutex
-	iowaitMode bool
+	pvs map[string]pvdata
 }
 
 type request struct {
@@ -201,25 +74,130 @@ type pluginSpec struct {
 	APIVersion  string   `json:"api_version,omitempty"`
 }
 
-func (p *Plugin) makeReport() (*report, error) {
-	metrics, err := p.metrics()
+// Iops struct
+type Iops struct {
+	Status string `json:"status"`
+	Data   struct {
+		ResultType string `json:"resultType"`
+		Result     []struct {
+			Metric struct {
+				Name              string `json:"__name__"`
+				Instance          string `json:"instance"`
+				Job               string `json:"job"`
+				KubernetesPodName string `json:"kubernetes_pod_name"`
+				OpenebsPv         string `json:"openebs_pv"`
+			} `json:"metric"`
+			Value []interface{} `json:"value"`
+		} `json:"result"`
+	} `json:"data"`
+}
+
+// main function
+func main() {
+	// creates the in-cluster config
+	config, err := rest.InClusterConfig()
 	if err != nil {
-		return nil, err
+		panic(err.Error())
+	}
+
+	// creates the clientset
+	clientset, err = kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// gets Url
+	url = os.Getenv("CORTEXAGENT")
+	if url == "" {
+		panic("Unable to retrieve the URL")
+	}
+
+	// we put the in a sub-directory to have more control on the permissions
+	const socketPath = "/var/run/scope/plugins/iops/iops.sock"
+
+	// Handle the exit signal
+	setupSignals(socketPath)
+	listener, err := setupSocket(socketPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	readch = make(chan map[string]float64)
+	writech = make(chan map[string]float64)
+
+	defer func() {
+		listener.Close()
+		os.RemoveAll(filepath.Dir(socketPath))
+	}()
+
+	plugin, err := NewPlugin()
+	if err != nil {
+		log.Fatalf("Failed to create a plugin: %v", err)
+	}
+	http.HandleFunc("/report", plugin.Report)
+	if err := http.Serve(listener, nil); err != nil {
+		log.Printf("error: %v", err)
+	}
+}
+
+func setupSocket(socketPath string) (net.Listener, error) {
+	os.RemoveAll(filepath.Dir(socketPath))
+	if err := os.MkdirAll(filepath.Dir(socketPath), 0700); err != nil {
+		return nil, fmt.Errorf("failed to create directory %q: %v", filepath.Dir(socketPath), err)
+	}
+	listener, err := net.Listen("unix", socketPath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to listen on %q: %v", socketPath, err)
+	}
+	log.Printf("Listening on: unix://%s", socketPath)
+	return listener, nil
+}
+
+func setupSignals(socketPath string) {
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-interrupt
+		os.RemoveAll(filepath.Dir(socketPath))
+		os.Exit(0)
+	}()
+}
+
+// NewPlugin instantiates a new plugin
+func NewPlugin() (*Plugin, error) {
+	plugin := &Plugin{
+		pvs: getPVs(),
+	}
+	return plugin, nil
+}
+
+func getValue(body []byte) (*Iops, error) {
+	storeBefore := new(Iops)
+	err := json.Unmarshal(body, &storeBefore)
+	if err != nil {
+		fmt.Println("whoops:")
+	}
+	return storeBefore, err
+}
+
+func (p *Plugin) makeReport() (*report, error) {
+	go p.updatePVs()
+	resource := make(map[string]node)
+	for k, v := range p.pvs {
+		resource[p.getTopologyPv(k)] = node{
+			Metrics: p.metrics(v.read, v.write),
+		}
 	}
 	rpt := &report{
 		PersistentVolume: topology{
-			Nodes: map[string]node{
-				p.getTopologyPv(): {
-					Metrics: metrics,
-				},
-			},
+			Nodes:           resource,
 			MetricTemplates: p.metricTemplates(),
 		},
 		Plugins: []pluginSpec{
 			{
-				ID:          "iowait",
-				Label:       "iowait",
-				Description: "Adds a graph of CPU IO Wait to Pod",
+				ID:          "iops",
+				Label:       "iops",
+				Description: "Adds a graph of read and write IOPS to PV",
 				Interfaces:  []string{"reporter"},
 				APIVersion:  "1",
 			},
@@ -228,32 +206,46 @@ func (p *Plugin) makeReport() (*report, error) {
 	return rpt, nil
 }
 
-func (p *Plugin) metrics() (map[string]metric, error) {
-	value := 33.66
-	id, _ := p.metricIDAndName()
+// Create the Metrics type on top-left side
+func (p *Plugin) metrics(read, write float64) map[string]metric {
 	metrics := map[string]metric{
-		id: {
+		"r": {
 			Samples: []sample{
 				{
 					Date:  time.Now(),
-					Value: value,
+					Value: read,
 				},
 			},
 			Min: 0,
-			Max: 100,
+			Max: 20,
+		},
+		"w": {
+			Samples: []sample{
+				{
+					Date:  time.Now(),
+					Value: write,
+				},
+			},
+			Min: 0,
+			Max: 20,
 		},
 	}
-	return metrics, nil
+	return metrics
 }
 
 func (p *Plugin) metricTemplates() map[string]metricTemplate {
-	id, name := p.metricIDAndName()
 	return map[string]metricTemplate{
-		id: {
-			ID:       id,
-			Label:    name,
-			Format:   "percent",
+		"r": {
+			ID:       "r",
+			Label:    "Read",
+			Format:   "percentage",
 			Priority: 0.1,
+		},
+		"w": {
+			ID:       "w",
+			Label:    "Write",
+			Format:   "percentage",
+			Priority: 0.2,
 		},
 	}
 }
@@ -261,8 +253,6 @@ func (p *Plugin) metricTemplates() map[string]metricTemplate {
 // Report is called by scope when a new report is needed. It is part of the
 // "reporter" interface, which all plugins must implement.
 func (p *Plugin) Report(w http.ResponseWriter, r *http.Request) {
-	p.lock.Lock()
-	defer p.lock.Unlock()
 	log.Println(r.URL.String())
 	rpt, err := p.makeReport()
 	if err != nil {
@@ -270,6 +260,7 @@ func (p *Plugin) Report(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
+
 	raw, err := json.Marshal(*rpt)
 	if err != nil {
 		log.Printf("error: %v", err)
@@ -280,10 +271,7 @@ func (p *Plugin) Report(w http.ResponseWriter, r *http.Request) {
 	w.Write(raw)
 }
 
-func (p *Plugin) getTopologyPv() string {
-	return fmt.Sprintf("%s;<persistent_volume>", p.PersistentVolumeID)
-}
-
 func (p *Plugin) metricIDAndName() (string, string) {
 	return "iops", "Iops"
 }
+
